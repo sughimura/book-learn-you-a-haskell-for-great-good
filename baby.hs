f4957a70970ff7bbb2f79b92ea5e92d56922f0f4
noritsugu sugimura <nxsughi@gmail.com>
2025-11-15 16:52:04 +0900

    feat: add baby.hs

---
+++ baby.hs
@@ -0,0 +1,8 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+  then x
+  else x*2
+doubleSmallNumber' x = doubleSmallNumber x + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
