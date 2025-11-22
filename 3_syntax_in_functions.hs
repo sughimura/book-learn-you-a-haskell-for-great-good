861ecf3d6b1a49c85ba515a000723861e32981e3
sughimura <nxsughi@gmail.com>
2025-11-22 11:06:25 +0900

    feat: add 3.3

--- 3_syntax_in_functions.hs
+++ 3_syntax_in_functions.hs
@@ -71,3 +71,37 @@ a `myCompare` b
     | a == b = EQ
     | a <= b = LT
     | otherwise = GT
+
+
+-- 3.3
+bmiTell'' :: Double -> Double -> String
+bmiTell'' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you, "
+
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+-- whereブロックの中の関数
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
